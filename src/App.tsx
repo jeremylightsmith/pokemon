91684c60c0425bf1ea50992e8f2df1f62fdf884c
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2024-05-28 10:37:16 -0700

    cleaning up styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import "./App.css";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { Link, Outlet, HashRouter, Routes, Route } from "react-router-dom";
 import Root from "./routes/root";
 import Play from "./routes/play";
 import BuildDeck from "./routes/build_deck";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/play",
-    element: <Play />,
-  },
-  {
-    path: "/build",
-    element: <BuildDeck />,
-  },
-]);
+const Layout = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <nav className="w-full flex justify-between">
+        <div>Pokemon TCG Simulator</div>
+        <ul className="flex gap-2">
+          <li>
+            <Link to="/play">Play</Link>
+          </li>
+          <li>
+            <Link to="/build">Build</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+};
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Root />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="/build" element={<BuildDeck />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
